3d61a2ce908460f793b059f707a3dd2f98a55096
George L <gogishvilli001@gmail.com>
2021-04-20 11:32:01 +0300

    Improve hr process

--- Skillbox8/Model/Department.cs
+++ Skillbox8/Model/Department.cs
@@ -1,35 +1,36 @@
 ﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Skillbox.App.Tools;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
 using System.Runtime.Serialization;
 
 namespace Skillbox.App.Model
 {
-    public class Department
+    public class Department : Observable
     {
         private readonly ObservableHashSet<Employee> employees = new ObservableHashSet<Employee>();
+        public static Dictionary<int, Department> AllDepartments = new Dictionary<int, Department>();
+
+        public int Id { get; internal set; } = AllDepartments.Count;
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        [IgnoreDataMember]
+        public ISet<Employee> Employees { get => employees; }
+        public ISet<int> EmployeeIDs => new HashSet<int>(employees.Select(x => x.Id));
+        public ISet<Department> Departments { get; } = new HashSet<Department>();
+
         public Department()
         {
             employees.CollectionChanged += Employees_CollectionChanged;
+            AllDepartments[Id] = this;
         }
-
-        public string Name { get; set; }
-        public DateTime Created { get; set; }
-        public int EmployeesCount => Employees.Count;
-
-        [IgnoreDataMember]
-        public ISet<Employee> Employees { get => employees; }
-        public ISet<Department> Departments { get; } = new HashSet<Department>();
         private void Employees_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            if (e.NewItems != null)
-                foreach (Employee newbie in e.NewItems)
-                    newbie.Department = this;
-            if (e.OldItems != null)
-                foreach (Employee retiree in e.OldItems)
-                    retiree.Department = null;
-            //TODO notify count changed
+            OnPropertyChanged(nameof(Employees));
+            OnPropertyChanged(nameof(EmployeeIDs));
         }
     }
 }
--- Skillbox8/Model/Employee.cs
+++ Skillbox8/Model/Employee.cs
@@ -1,24 +1,44 @@
-﻿namespace Skillbox.App.Model
+﻿using Skillbox.App.Tools;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Skillbox.App.Model
 {
-    public class Employee
+    public class Employee : Observable
     {
         private Department department;
 
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
+        public decimal Salary { get; set; }
+        public int DepartmentId
+        {
+            get => department.Id;
+            set
+            {
+                Debug.WriteLine($"Employee {Id}.DepartmentId is assigned {value}");
+                Department = Department.AllDepartments.Single(x => x.Key == value).Value;
+            }
+        }
+
+        [IgnoreDataMember]
         public Department Department
         {
             get => department;
             set
             {
-                if (department == value) return;
+                if (department == value)
+                    return;
+                Debug.WriteLine($"Employee {Id}.Department is assigned {value.Id}");
                 department?.Employees.Remove(this);
                 department = value;
                 department.Employees.Add(this);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(DepartmentId));
             }
         }
-        public int Id { get; set; }
-        public decimal Salary { get; set; }
     }
 }
